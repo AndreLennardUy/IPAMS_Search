0f3a38e01b7c5f14e76265311cb6c311d93bc45e
Victor Emmanuel F. Garcia <victoremmanuel.garcia@cit.edu>
2023-05-29 21:49:39 +0800

    Modify model to add PSCED

--- Backend/search/records/models.py
+++ Backend/search/records/models.py
@@ -7,3 +7,11 @@ class RecordModel(models.Model):
     psed_classification_id = models.IntegerField(max_length=11)
     for_commercialization = models.IntegerField(max_length=1)
     date_uploaded = models.DateTimeField()
+
+class PSCEDClassification(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
