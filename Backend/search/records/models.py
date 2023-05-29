da81767dc6c2f0aa9e9b0aa30f0a9392e5f71ca8
Victor Emmanuel F. Garcia <victoremmanuel.garcia@cit.edu>
2023-05-29 22:33:29 +0800

    Add psedclassification

--- Backend/search/records/models.py
+++ Backend/search/records/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
+class PSEDClassification(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    
 class RecordModel(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
@@ -8,10 +18,8 @@ class RecordModel(models.Model):
     for_commercialization = models.IntegerField(max_length=1)
     date_uploaded = models.DateTimeField()
 
-class PSCEDClassification(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    date_created = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
-        return self.name
+        return self.title
+
+
+
