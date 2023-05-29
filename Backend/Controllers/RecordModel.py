1c4fc355fc48cd98fe8775e96ec40bcf7cc7b8fe
John William Miones <johnwilliam.miones@cit.edu>
2023-05-29 20:22:46 +0800

    Add django ipams

---
+++ Backend/Controllers/RecordModel.py
@@ -0,0 +1,7 @@
+class RecordModel:
+    def __init__(self, id, title, abstract, author, field):
+        self.id = id
+        self.title = title
+        self.abstract = abstract
+        self.author = author
+        self.field = field
