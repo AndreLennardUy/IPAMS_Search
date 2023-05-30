c7fd677accb776d2ed6257424b07dd5ea4e88b98
John William Miones <johnwilliam.miones@cit.edu>
2023-05-30 09:45:05 +0800

    Delete unuse files

--- Backend/Controllers/RecordModel.py
+++
@@ -1,8 +0,0 @@
-class RecordModel:
-    def __init__(self, id, title, abstract, author, field):
-        self.id = id
-        self.title = title
-        self.abstract = abstract
-        self.author = author
-        self.field = field
-        self.abstract = id
--- Backend/Models/record.py
+++
@@ -1,7 +0,0 @@
-    def __init__(self, title, abstract, record_id):
-        self.title = title
-        self.abstract = abstract
-        self.id = record_id
-
-    def __repr__(self):
-        return f"Record(title='{self.title}', abstract='{self.abstract}', id='{self.id}')"
